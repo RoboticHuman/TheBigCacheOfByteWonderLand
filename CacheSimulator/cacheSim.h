cfc935d56b2aef61bec560e0575844aeb950ac8b
IbrahimSba3i <el-sebaie.ebrahim@aucegypt.edu>
2013-12-18 17:46:21 +0200

    platforms folder

--- CacheSimulator/cacheSim.h
+++ CacheSimulator/cacheSim.h
@@ -6,6 +6,37 @@
 #include <fstream>
 using namespace std;
 
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+//\\	 BETA
+//\\	
+//\\	
+//\\~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//\\	1 - Install the font attached(for a better experience)
+//\\	2 - If you want to try it run the file CacheSimulator.exe
+//\\	3 - For the source code you'll find 42 attached files that include the simulator and the graphical user interface code.
+//\\
+//\\
+//\\	The files for the base simulator are :
+//\\	cacheSim.cpp
+//\\	cacheSim.h
+//\\	cacheline.h
+//\\
+//\\	The remaining files are for the Qt interface.
+//\\
+//\\	Github repository link : https ://github.com/RoboticHuman/TheBigCacheOfByteWonderLand.git
+//\\
+//\\
+//\\~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//\\	This program has been made for the course CSCE 231 under the supervision of Dr.Mohamed Shalan
+//\\
+//\\	Ibrahim ElSebaie - 900120478
+//\\	Omar ElBehady - 900121388
+//\\	Hassan ElTahan - 900101697
+//\\
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
+
 enum condition { MISS, HIT };
 enum cacheType { DirectMapped, FullyAssociative };
 enum replacementMethod { RANDOM , FIFO, LFU };
